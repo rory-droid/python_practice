8c3759edd15df6ef1d920ba8633202950fa0f0fb
rory-droid <57977991+rory-droid@users.noreply.github.com>
2019-12-07 22:14:43 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("botha these")
+Q = input()
+if Q == "Both of which?":
+    print("DEEZ NUTS")
+elif Q == "Both of what?":
+    print("DEEZ NUTS")
+elif Q == "?":
+    print("DEEZ NUTS")
+elif Q == "what?":
+    print("DEEZ NUTS")
+else:
+    print("DEEZ NUTS")
